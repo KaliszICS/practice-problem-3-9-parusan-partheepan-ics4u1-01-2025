f98a3a2d96d3c44fbdbf5f5d6108554a48f5a7d3
Parusan <Parusan.partheepan@gmail.com>
2025-11-11 13:17:56 -0500

    make changes

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,28 @@
+import java.util.ArrayList;
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+    public static void main(String[] args) {
+        String[] result = towerOfHanoi(3);
+        for (String move : result) {
+            System.out.println(move);
+        }
+    }
 
-	}
+    public static String[] towerOfHanoi(int n) {
+        if (n <= 0) return new String[0];
+        ArrayList<String> moves = new ArrayList<>();
+        solve(n, 'L', 'M', 'R', moves);
+        return moves.toArray(new String[0]);
+    }
 
-	public static void q1() {
-		//Write question 1 code here
-	}
-
-	public static void q2() {
-		//Write question 2 code here
-	}
-
-	public static void q3() {
-		//Write question 3 code here
-	}
-
-	public static void q4() {
-		//Write question 4 code here
-	}
-
-	public static void q5() {
-		//Write question 5 code here
-	}
-
+    private static void solve(int n, char from, char aux, char to, ArrayList<String> moves) {
+        if (n == 1) {
+            moves.add("" + from + to);
+            return;
+        }
+        solve(n - 1, from, to, aux, moves);
+        moves.add("" + from + to);
+        solve(n - 1, aux, from, to, moves);
+    }
 }
